587df6dfcdea843262f21f3f704c4b14a7b8fa3e
molor824 <molormargaderdene@gmail.com>
2024-11-01 10:01:49 +0800

    fix: form not closing when submit clicked

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,17 @@ import Modal from "./Modal";
 
 function Header({ onNewPost }) {
   const [formOpen, setFormOpen] = useState(false);
+  const handleSubmit = (post) => {
+    onNewPost(post);
+    handleCloseRequest();
+  };
   const handleCloseRequest = () => setFormOpen(false);
   const handleOpenRequest = () => setFormOpen(true);
 
   return (
     <header className="flex flex-row items-center justify-between w-full max-w-[64rem] p-4">
       <Modal open={formOpen} onCloseRequested={handleCloseRequest}>
-        <Newform onSubmit={onNewPost} />
+        <Newform onSubmit={handleSubmit} />
       </Modal>
       <h1 className="text-2xl text-white">React Poster</h1>
       <button
